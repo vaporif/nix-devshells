1a68f50c35b813b8bd7b1b9df295e752bc5085be
Dmytro Onypko <vaporif@gmail.com>
2025-03-21 22:58:11 +0000

    init

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Nix for rust development";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    fenix.url = "github:nix-community/fenix";
+    fenix.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ flake-parts, fenix, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        ./nix/devshells.nix
+      ];
+      systems = [ "x86_64-linux" "aarch64-darwin" ];
+
+      perSystem = { system, pkgs, ... }: {
+        # per-system attributes can be defined here. the self' and inputs'
+        # module parameters provide easy access to attributes of the same
+        # system.
+        _module.args.pkgs = import inputs.nixpkgs {
+          inherit system;
+          overlays = [ fenix.overlays.default ];
+        };
+
+        formatter = pkgs.nixpkgs-fmt;
+      };
+    };
+}
---
+++ nix/devshells.nix
@@ -0,0 +1,27 @@
+{  ... }: {
+  perSystem = {  pkgs, ... }:
+    let
+      rust = pkgs.fenix.stable;
+      rustToolchain = pkgs.fenix.combine [
+        (rust.withComponents [
+          "cargo"
+          "clippy"
+          "rustc"
+          "rust-analyzer"
+        ])
+        pkgs.fenix.latest.rustfmt
+      ];
+    in
+    {
+      devShells = {
+        default = pkgs.mkShell {
+          packages = with pkgs; [ cargo-make pkg-config rustToolchain ] ++ [
+          ];
+          shellHook = ''
+            export RUST_SRC_PATH="${rust.rust-src}/lib/rustlib/src/rust/library";
+            export PATH=$HOME/.cargo/bin:$PATH
+          '';
+        };
+      };
+    };
+}
